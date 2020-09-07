0cab7b210c6be412554421260753b014566767d3
Goh Rui Kun <61505106+rkapril@users.noreply.github.com>
2020-09-08 00:01:50 +0800

    Add files via upload

---
+++ blockchain/main.py
@@ -0,0 +1,142 @@
+# my_name = input('Please enter your name: ')
+# print(my_name)
+#
+# file = open('name.txt', mode='w')
+# print(file)
+# file.write(my_name)
+#
+# file.close()
+# file = open('name.txt', mode='r')
+# print(file.read())
+
+# # variable
+# age = 29
+# print(age)
+#
+# age = 30
+# print(age)
+#
+# age = age - 1
+# print(age)
+#
+# age = 29.5
+# print(age)
+#
+# is_old = True
+# print(is_old)
+#
+# name = 'Max'
+# print(name)
+
+# Number
+# age = '29'
+# age = age + '1'
+# print(age)
+#
+# print('Hello' + ' World')
+#
+# age = '29'
+# print(int(age) + 1)
+#
+# print(int(1.5))
+#
+# print(float(age))
+# print(float(age) + 1)
+# print(float(True))
+# print(float(False))
+
+# Operators
+# print("Hello " + "World")
+# greeting = "Hello " + "World"
+# print(greeting)
+#
+# print("Hello " * 10)
+# print("_" * 30)
+
+# Basics
+
+# print(1e10)
+# print(1e6)
+# print(1 - 0.9)
+# print(1 - 0.5)
+# print(1 - 0.9 - 0.1)
+#
+# name = "Max"
+# name = "I'm pretty cool!"
+# print(name)
+#
+# name = 'I\'m pretty cool!'
+# print(name)
+#
+# longer_text = """you can add your text """
+# print(longer_text)
+# longer_text = """you can add your text
+# ... which can span multiple lines
+# ... !!! """
+# print(longer_text)
+
+# lists
+# blockchain = [1, 8.6, 5.1]
+# print(blockchain)
+# print(blockchain[1])
+# print(blockchain[1] + 2)
+# print(blockchain)
+# blockchain = [1, 8.6, 5.1, 10]
+# print(blockchain)
+# blockchain.append(3)
+# print(blockchain)
+# print(blockchain.pop())
+# print(blockchain)
+
+# def greet():
+#     print('Hello')
+#
+#
+# greet()
+
+# def greet(name):
+#     print('Hello ' + name)
+#
+#
+# greet('Max')
+
+# def sum(a, b):
+# #     return a + b
+# #
+# # print(sum(2,5))
+
+# def greet(name, age=29):
+#     print('Hello ' + name + ', I am ' + str(age))
+#
+#
+# greet('Max')
+
+# def greet(name, age):
+#     print('Hello ' + name + ', I am ' + str(age))
+#
+#
+# greet(age=29, name='Max')
+
+
+# name = 'Max'
+#
+#
+# def greet():
+#     age = 29
+#     print('Hi ' + name + ', I am ' + str(age))
+#
+#
+# greet()
+
+
+# global name
+# name = 'Max'
+#
+#
+# def get_name():
+#     global name
+#     name = input('Your name: ')
+#
+#
+# get_name()
+# print(name)
